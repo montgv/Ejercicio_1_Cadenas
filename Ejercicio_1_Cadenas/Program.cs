bbdeefc95e13e84b1543a26ab54a6911caacecbb
estudiante <montserrat.garrido.220@estudiante.iesoretania.es>
2022-10-13 14:39:19 +0200

    Agregar archivos de proyecto.

---
+++ Ejercicio_1_Cadenas/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace Ejercicio_1_Cadenas
+{
+    class Ejercicio_1_Cadenas
+    {
+        static void Main(String[] args)
+        {
+            string entrada = "";
+            Console.WriteLine("Escribe una cadena: ");
+            entrada = Console.ReadLine();
+
+            foreach(String caracter : entrada) {
+
+            }
+        }
+    }
+}
