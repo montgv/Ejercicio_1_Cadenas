6ed02c0ec44c4196eff7de662656aeb4e1eabad7
Montse <montserrat.garrido.220@estudiante.iesoretania.es>
2022-10-20 17:50:19 +0200

    Commit final

--- Ejercicio_1_Cadenas/Program.cs
+++ Ejercicio_1_Cadenas/Program.cs
@@ -4,7 +4,7 @@
     {
         static void Main(String[] args)
         {
-            /*//Escribe por pantalla caracter a caracter la cadena introducida por teclado
+            //Escribe por pantalla caracter a caracter la cadena introducida por teclado
             Console.WriteLine("Ejercicio 1: ");
             Console.WriteLine("Escribe una cadena: ");
             string entrada1 = Console.ReadLine();
@@ -36,21 +36,22 @@
             Console.WriteLine("Escribe una cadena: ");
             string entrada3 = Console.ReadLine();
 
-            char buscar;
-            int contador = 0;
+            string buscar3;
+            int contador3 = 0;
 
             Console.WriteLine("Escribe un carácter a buscar: ");
-            buscar = (char) Console.Read();
-            if (Char.IsLetter(buscar))
+            buscar3 = Console.ReadLine();
+            char caracterBuscar3 = buscar3.ToCharArray()[0];
+            if (Char.IsLetter(caracterBuscar3))
             {
                  foreach (char caracter in entrada3)
                  {
-                     if (caracter.Equals(buscar))
+                     if (caracter.Equals(caracterBuscar3))
                      {
-                         contador++;
+                         contador3++;
                      }
                  }
-                 Console.WriteLine("El carácter aparece " + contador + " veces.");
+                 Console.WriteLine("El carácter aparece " + contador3 + " veces.");
             } else
                 {
                     Console.WriteLine("Carácter no valido.");
@@ -62,34 +63,36 @@
             Console.WriteLine("Escribe una frase: ");
             string entrada4 = Console.ReadLine();
 
-            string[] palabra = entrada4.Split(' ');
+            string[] palabra4 = entrada4.Split(' ');
 
-            Console.WriteLine("Tiene " + palabra.Length + " palabras.");
+            Console.WriteLine("Tiene " + palabra4.Length + " palabras.");
             Console.WriteLine();
 
             //Muestra las iniciales del nombre guardado en mayusculas
             Console.WriteLine("Ejercicio 5: ");
-            string nombreCompleto = "juan antonio garcia perez";
-            string[] partesNombre = nombreCompleto.Split(' ');
-            for (int i = 0; i < partesNombre.Length; i++)
+            string nombreCompleto5 = Console.ReadLine();
+
+            string[] partesNombre5 = nombreCompleto5.Split(' ');
+            string iniciales = "";
+            foreach (string nombreSeparado in partesNombre5)
             {
-                for (int j = 0; j < partesNombre[i].Length; j++)
-                {
-                    partesNombre[i][0] = partesNombre[i][0].T
-                }
+                iniciales += nombreSeparado.ToUpper().ToCharArray()[0];
             }
+            Console.WriteLine(iniciales);
+            Console.WriteLine();
 
             //Muestra una cadena de caracteres que es el resultado de invertir otra cadena
             Console.WriteLine("Ejercicio 6: ");
             Console.WriteLine("Escribe una cadena: ");
             string entrada6 = "";
             entrada6 = Console.ReadLine();
-            string cadenaInvertida = "";
+            string cadenaInvertida6 = "";
             for (int i = 0; i < entrada6.Length; i++)
             {
-                cadenaInvertida = entrada6[i] + cadenaInvertida;
+                cadenaInvertida6 = entrada6[i] + cadenaInvertida6;
             }
-            Console.WriteLine(cadenaInvertida);*/
+            Console.WriteLine(cadenaInvertida6);
+            Console.WriteLine();
 
             //Sustituye el caracter a buscar por el indicado en la cadena dada
             Console.WriteLine("Ejercicio 7: ");
@@ -98,21 +101,24 @@
             entrada7 = Console.ReadLine();
 
             Console.WriteLine("Escribe el carácter que deseas cambiar: ");
-            char buscar = (char)Console.Read();
+            string buscar7 = Console.ReadLine();
+            char carBuscar7 = buscar7.ToCharArray()[0];
 
             Console.WriteLine("Escribe el carácter que desear poner en su lugar: ");
-            char cambiar = (char)Console.Read();
+            string cambiar7 = Console.ReadLine();
+            char carCambiar7 = buscar7.ToCharArray()[0];
 
             string cadenaFinal = "";
 
-            if (Char.IsLetter(buscar) && Char.IsLetter(cambiar))
+            if (Char.IsLetter(carBuscar7) && Char.IsLetter(carCambiar7))
             {
-                cadenaFinal = entrada7.Replace(buscar, cambiar);
+                cadenaFinal = entrada7.Replace(carBuscar7, carCambiar7);
             }
             else
             {
                 Console.WriteLine("Carácter no valido.");
             }
+            Console.WriteLine(cadenaFinal);
             Console.WriteLine();
 
             //Convierte las mayusculas en minusculas y al reves en una cadena introducida por teclado
@@ -120,34 +126,39 @@
             Console.WriteLine("Escribe una cadena: ");
             string entrada8 = "";
             entrada8 = Console.ReadLine();
+            string resultado8 = "";
 
-            foreach (string caracter in entrada8)
+            foreach (char caracter in entrada8)
             {
-                if (caracter.Equals(caracter.ToUpper))
+                if (char.IsUpper(caracter))
                 {
-
+                    resultado8 += Char.ToLower(caracter);
+                } else
+                {
+                    resultado8 += Char.ToUpper(caracter);
                 }
             }
+            Console.WriteLine(resultado8);
             Console.WriteLine();
 
-            //Comprueba si la cadena introducida contiene la subcadena introducida
+            // Comprueba si la cadena introducida contiene la subcadena introducida
             Console.WriteLine("Ejercicio 9: ");
             Console.WriteLine("Escribe una cadena: ");
             string entrada9 = "";
             entrada9 = Console.ReadLine();
 
             Console.WriteLine("Escribe la subcadena a buscar: ");
-            string subcadena = "";
-            subcadena = Console.ReadLine();
+            string subcadena9 = "";
+            subcadena9 = Console.ReadLine();
 
-            if (entrada9.Contains(subcadena))
+            if (entrada9.Contains(subcadena9))
             {
                 Console.WriteLine("La cadena contiene la subcadena.");
             } else
             {
                 Console.WriteLine("La cadena no contiene la subcadena.");
-
             }
+            Console.WriteLine();
 
             //Comprobar si la cadena introducida des un palindromo
             Console.WriteLine("Ejercicio 10: ");
@@ -155,6 +166,19 @@
             string entrada10 = "";
             entrada10 = Console.ReadLine();
 
+            string cadenaInvertida10 = "";
+            for (int i = 0; i < entrada10.Length; i++)
+            {
+                cadenaInvertida10 = entrada10[i] + cadenaInvertida10;
+            }
+
+            if (entrada10.Equals(cadenaInvertida10))
+            {
+                Console.WriteLine("La cadena '" + entrada10 + "' es un palindromo.");
+            } else
+            {
+                Console.WriteLine("La cadena '" + entrada10 + "' no es un palindromo.");
+            }
         }
     }
 }
